1da7c91382271621eda2d809eb653449f0799a30
Magalzin <matheusschorro@gmail.com>
2025-10-06 15:35:46 -0300

    Fixing module

--- src/new_products/new_products.module.ts
+++ src/new_products/new_products.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { NewProductsService } from './new_products.service';
 import { NewProductsController } from './new_products.controller';
 import { NewProduct } from './entity/new_product.model';
-import { CryptoService } from 'src/common/crypto_service.service';
+import { CryptoService } from '../common/crypto_service.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
